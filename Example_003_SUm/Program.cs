84fae927f094abd47c2b89326e2012cb56d43b06
dzz_cat <dzz_cat@mail.ru>
2023-04-06 13:00:19 +0400

    добавлен запуск суммы рандомного числа

--- Example_003_SUm/Program.cs
+++ Example_003_SUm/Program.cs
@@ -1,11 +1,22 @@
-﻿
+﻿// нахождение суммы двух чисел
 // вводим переменную, где будет хранится число
-int numberA = 32;
+// int numberA = 32;
 //вводим переменную второго числа
-int numberB = 5;
-// выводим н в консоль сумму  оба числа
+//int numberB = 5;
+
+//нахождение случайного целого числа
+int numberA = new Random().Next(1, 10); //случайное число 0,1,2....9
+Console.WriteLine(numberA); //выводим сгенерированное число в консоль
+int numberB = new Random().Next(1, 10); //случайное число 0,1,2....9
+Console.WriteLine(numberB); //выводим сгенерированное число в консоль
+
+// выводим  в консоль сумму  числел
 // Console.WriteLine(numberA + numberB);
+
 // упрощаем код сохранение результата в доп переменную
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+
+
 
