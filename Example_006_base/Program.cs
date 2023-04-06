6ff2d72d8e1387dd78b83a787ebe0aae35c39403
dzz_cat <dzz_cat@mail.ru>
2023-04-06 16:10:19 +0400

    add project Example_006_base

---
+++ Example_006_base/Program.cs
@@ -0,0 +1,19 @@
+﻿// Нахождение максимального числа из имеющихся. Задача с гирями
+
+// прописываем целые числа из задачи
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a; // если  в значении а - максимум, то положить ее в значение переменной max
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
