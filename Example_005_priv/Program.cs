76d036d7f1df0d9aa0a1fbe0ff43c6ac868139ad
dzz_cat <dzz_cat@mail.ru>
2023-04-06 13:26:53 +0400

    add projecy 005 (if/else)

---
+++ Example_005_priv/Program.cs
@@ -0,0 +1,20 @@
+﻿//использование операторов If/else
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine(); // вводим строчное имя переменной = и считываем данные 
+
+
+//проверка 
+//if (username == "Маша") 
+
+//ToLower  позволяет все симфолы строки перевести в нижний регистр 
+if (username.ToLower() == "маша")
+{  
+    Console.WriteLine("Ура! Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    
+}
