6088925da8db8dd3151b953b83d433a56487cd05
dzz_cat <dzz_cat@mail.ru>
2023-04-06 12:24:33 +0400

    add sum

--- Example_003_SUm/Program.cs
+++ Example_003_SUm/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// вводим переменную, где будет хранится число
+int numberA = 32;
+//вводим переменную второго числа
+int numberB = 5;
+// выводим н в консоль сумму  оба числа
+// Console.WriteLine(numberA + numberB);
+// упрощаем код сохранение результата в доп переменную
+int result = numberA + numberB;
+Console.WriteLine(result);
+
