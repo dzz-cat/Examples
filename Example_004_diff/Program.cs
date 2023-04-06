2e54e87bd8cdac73a1b12f73dcfe19d789ef0498
dzz_cat <dzz_cat@mail.ru>
2023-04-06 12:41:19 +0400

    add code in file

--- Example_004_diff/Program.cs
+++ Example_004_diff/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// работает только для целых чисел
+// int numberA = 12; 
+// int numberB = 5;
+// контейнер double позволяет работать с вещественными числами
+double numberA = 12;
+double numberB = 5;
+
+Console.WriteLine(numberA / numberB);
